80c471df1a2a73942dab0c8fff789f15577768ec
Michael.Keller <thorinn_e@msn.com>
2020-06-01 08:22:07 -0600

    First chpater file

---
+++ CH1.py
@@ -0,0 +1,15 @@
+# CH 1 Challenges
+# Problem 1
+
+icecream = input("What is your favorite ice cream: ")
+print(f"Your favorit ice cream is {icecream}!\n\n")
+
+# Problem 2
+name = input("What is your name? ")
+print(f"Your name is {name.title()}.\n")
+input("Press enter to continue\n\n")
+
+
+# Problem 3
+print("Make my day....PUNK!")
+print("Clint Eastwood")
